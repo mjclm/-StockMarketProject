449a2823d83348def682e87d61e7dea5d796bfde
mickaeljuillet <mickajuillet@hotmail.fr>
2020-11-19 16:46:11 +0100

    Twitter API

---
+++ stock/collect_twitter.py
@@ -0,0 +1,64 @@
+import requests
+
+import yaml
+
+# Buildings queries
+# Boolean syntax to group multiple operators
+# (Standalone operators not recommended)
+
+# Successive operators with a space btw them means "AND" logic.
+# Ex: snow day #NoSchool -> Tweets with all the keywords
+
+# Successive operators with a OR btw them means "OR" logic.
+# Ex: grumpy OR cat OR #meme -> Tweets with any of the keywords
+
+# Use parentheses to group operators together
+# use a dash to negate a keyword or operators grouped with parentheses.
+
+# Note:
+# queries max length: 512
+# All operators can be negated
+# Recommended to negate each individual operator instead to
+# negate a set of operators grouped together.
+
+# use bearer token
+
+
+def create_twitter_url():
+    handle = "#MSFT OR #AMZN OR #FB"
+    max_results = 10
+    fields = ['id', 'text', 'created_at',
+              'public_metrics', 'context_annotations', "geo"
+              ]
+    mrf = "max_results={}".format(max_results)
+    tf = "tweet.fields={}".format(",".join(fields))
+    start_time = "start_time={}".format("2020-11-15T01:00:00-05:00")
+    end_time = "end_time={}".format("2020-11-16T01:00:00-05:00")
+    q = "query={}".format(handle)
+    url = "https://api.twitter.com/2/tweets/search/recent?{}&{}&{}&{}&{}".format(
+        # start_time, end_time,
+        mrf, tf, q
+    )
+    return url
+
+
+def process_yaml():
+    with open("twitter_keys.yaml") as file:
+        return yaml.safe_load(file)
+
+
+def create_bearer_token(data):
+    return data["bearer_token"]
+
+
+def twitter_auth_and_connect(bearer_token, url):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
+
+
+def give_tweets():
+    url = create_twitter_url()
+    data = process_yaml()
+    bearer_token = create_bearer_token(data)
+    return twitter_auth_and_connect(bearer_token, url)
