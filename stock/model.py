06f2dd14b622327a6498c23e9e4b77d909c25eaf
mickaeljuillet <mickajuillet@hotmail.fr>
2020-12-01 17:21:54 +0100

    new test with sklearn api

--- stock/model.py
+++ stock/model.py
@@ -1,7 +1,13 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import RandomizedSearchCV
 
 
 def my_model(columns_to_scale):
@@ -9,7 +15,25 @@ def my_model(columns_to_scale):
         transformers=[('num', StandardScaler(), columns_to_scale)],
         remainder='passthrough')
 
-    clf = Pipeline(steps=[('preprocessor', preprocessor),
-                          ('classifier', RandomForestClassifier(class_weight='balanced', random_state=0))])
+    rf = RandomForestClassifier(random_state=1, class_weight='balanced')
+    lr = LogisticRegression(max_iter=500,  random_state=1, class_weight='balanced')
+    pipe_lr = Pipeline([
+        ('scale', StandardScaler()),
+        ('reduce_dims', PCA(n_components=10)),
+        ('clf', lr)
+    ])
 
-    return clf
+    vc = VotingClassifier(estimators=[('rf', rf), ('clf2', pipe_lr)], voting='hard')
+
+    pipe = Pipeline(steps=[('preprocessor', preprocessor), ('vc', vc)])
+
+    params = {
+        'vc__rf__n_estimators': [10, 20, 40, 100],
+        'vc__rf__max_depth': [5, 15, 25, 50],
+        'vc__rf__min_samples_split': [.1, .05, .02, .01],
+        'vc__clf2__clf__C': [1., .5, .1, .01]
+    }
+
+    search = RandomizedSearchCV(pipe, params, n_iter=20, random_state=1)
+
+    return search
